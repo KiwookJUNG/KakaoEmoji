717ac3bf46a339319f36a09eb8d51460c8ff00c6
KiwookJUNG <ismeeki17@naver.com>
2019-11-01 13:46:58 +0900

    Initial Commit

---
+++ KakaoEmoji/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KakaoEmoji
+//
+//  Created by 정기욱 on 2019/11/01.
+//  Copyright © 2019 kiwook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
