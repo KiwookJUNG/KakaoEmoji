7493d7f41f1283a86e539b6a124aa1ca6794ad22
KiwookJUNG <ismeeki17@naver.com>
2019-11-02 13:49:12 +0900

    스토리보드 뷰 설정 + 이미지피커 + Vision 이미지 분석

--- KakaoEmoji/ViewController.swift
+++ KakaoEmoji/ViewController.swift
@@ -7,9 +7,31 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
 class ViewController: UIViewController {
 
+    @IBAction func imgPick(_ sender: Any) {
+        
+       let albumPicker = UIImagePickerController()
+        albumPicker.allowsEditing = false
+        albumPicker.delegate = self
+        albumPicker.sourceType = .photoLibrary
+                     
+        albumPicker.modalPresentationStyle = .fullScreen
+                     
+        present(albumPicker, animated: true)
+        
+        
+    }
+    @IBOutlet weak var imgView: UIImageView!
+    
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    @IBOutlet weak var confidenceLbl: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -18,3 +40,67 @@ class ViewController: UIViewController {
 
 }
 
+extension ViewController : UIImagePickerControllerDelegate, UINavigationControllerDelegate  {
+    
+   func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        dismiss(animated: true, completion: nil)
+        
+        guard let image = info[.originalImage] as? UIImage else {
+            fatalError("이미지를 로드 할 수 없습니다.")
+        }
+        
+        self.imgView.image = image
+        
+        guard let ciImage = CIImage(image: image) else {
+            fatalError("UIImage를 CIImage로 전환할 수 없습니다.")
+        }
+        
+        // 이미지 분석
+        coreMLProcessing(image: ciImage)
+    }
+    
+}
+
+extension ViewController {
+    
+    func coreMLProcessing(image: CIImage) {
+        
+        // 모델 등록 - VNCoreMLModel(for:)
+        guard let model = try? VNCoreMLModel(for: KakaoEmoji_1().model) else {
+            fatalError("TubeApeach ML Model을 로드할 수 없습니다.")
+        }
+        
+        
+        
+        // 등록한 모델을 사용해서 이미지 분석을 요청 - VNCoreMLRequest(model:)
+        let request = VNCoreMLRequest(model: model) { [weak self] request, error in
+            
+            print(request.results)
+            guard let results = request.results as? [VNClassificationObservation] else {
+                return
+            }
+            guard let firstItem = results.first else {
+                return
+            }
+            
+            DispatchQueue.main.async { [weak self] in
+                self?.nameLbl.text = firstItem.identifier
+                self?.confidenceLbl.text = String(firstItem.confidence)
+            }
+        }
+        
+        
+        
+        // 핸들러로 요청 실행 - VNImageRequestHandler(ciImage:)
+        let handler = VNImageRequestHandler(ciImage: image, options: [:])
+        DispatchQueue.global(qos: .userInitiated).async {
+            try? handler.perform([request])
+        }
+        
+        
+    }
+}
