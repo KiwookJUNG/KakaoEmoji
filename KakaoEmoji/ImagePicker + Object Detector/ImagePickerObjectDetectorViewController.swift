0b28ae1da3706f875248110149e69d06562f34d5
KiwookJUNG <ismeeki17@naver.com>
2019-11-04 13:57:59 +0900

    Object Detection - 바운딩 박스 수정

--- KakaoEmoji/ImagePicker + Object Detector/ImagePickerObjectDetectorViewController.swift
+++ KakaoEmoji/ImagePicker + Object Detector/ImagePickerObjectDetectorViewController.swift
@@ -56,7 +56,7 @@ extension ImagePickerObjectDetector: UINavigationControllerDelegate, UIImagePick
            self.imgView.image = image
         
            self.imgWidth = Int(self.view.bounds.size.width)
-           self.imgHeight = Int(self.imgView.image?.size.height ?? 400)
+           self.imgHeight = Int(self.view.bounds.size.height - 200)
            
            guard let ciImage = CIImage(image: image) else {
                fatalError("UIImage를 CIImage로 전환할 수 없습니다.")
@@ -80,9 +80,15 @@ extension ImagePickerObjectDetector {
         // 등록한 모델을 사용해서 이미지 분석을 요청 - VNCoreMLRequest(model:)
         let request = VNCoreMLRequest(model: model) { [weak self] request, error in
             
-            //print(request.results)
+            print(request.results)
             guard let results = request.results as? [VNRecognizedObjectObservation] else {
                 return
+            }
+            if results.isEmpty {
+                DispatchQueue.main.async {
+                    self?.btnName.setTitle("인식 할 수 없음", for: .normal)
+                }
+                
             }
 //            guard let firstItem = results.first else {
 //                return
@@ -102,6 +108,7 @@ extension ImagePickerObjectDetector {
                         return
                     }
                 
+                    
 
                     
                     DispatchQueue.main.async { [weak self] in
@@ -139,11 +146,15 @@ extension ImagePickerObjectDetector {
         shapeLayer.bounds = bounds
         shapeLayer.position = CGPoint(x: bounds.midX, y: bounds.midY)
         shapeLayer.name = "Found Object"
-        shapeLayer.backgroundColor = color
+        //shapeLayer.backgroundColor = color
+        shapeLayer.borderWidth = 10.0
+        shapeLayer.borderColor = color
        
         shapeLayer.cornerRadius = 7
         return shapeLayer
     }
+    
+ 
     
     enum EmojiOrCat: String {
         case apeach = "Apeach"
@@ -159,14 +170,14 @@ extension ImagePickerObjectDetector {
     
     func boxColor(_ emojiOrCat: EmojiOrCat) -> CGColor {
         switch emojiOrCat {
-        case .apeach: return CGColor(srgbRed: 1.00, green: 0.00, blue: 0.72, alpha: 0.3)
-        case .tube: return CGColor(srgbRed: 0.66, green: 0.53, blue: 0.07, alpha: 0.3)
-        case .benny: return CGColor(srgbRed: 0.89, green: 0.70, blue: 0.70, alpha: 0.3)
-        case .taby: return CGColor(srgbRed: 0.20, green: 0.01, blue: 0.04, alpha: 0.3)
-        case .black: return CGColor(srgbRed: 0.53, green: 0.18, blue: 0.66, alpha: 0.3)
-        case .cheeze: return CGColor(srgbRed: 0.90, green: 0.88, blue: 0.39, alpha: 0.3)
-        case .ryan: return CGColor(srgbRed: 0.90, green: 0.88, blue: 0.00, alpha: 0.3)
-        case .white: return CGColor(srgbRed: 0.82, green: 0.93, blue: 0.93, alpha: 0.3)
+        case .apeach: return CGColor(srgbRed: 1.00, green: 0.00, blue: 0.72, alpha: 0.5)
+        case .tube: return CGColor(srgbRed: 0.66, green: 0.53, blue: 0.07, alpha: 0.5)
+        case .benny: return CGColor(srgbRed: 0.89, green: 0.70, blue: 0.70, alpha: 0.5)
+        case .taby: return CGColor(srgbRed: 0.20, green: 0.01, blue: 0.04, alpha: 0.5)
+        case .black: return CGColor(srgbRed: 0.53, green: 0.18, blue: 0.66, alpha: 0.5)
+        case .cheeze: return CGColor(srgbRed: 0.90, green: 0.88, blue: 0.39, alpha: 0.5)
+        case .ryan: return CGColor(srgbRed: 0.90, green: 0.88, blue: 0.00, alpha: 0.5)
+        case .white: return CGColor(srgbRed: 0.82, green: 0.93, blue: 0.93, alpha: 0.5)
         }
     }
 }
